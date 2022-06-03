e30ea5c53b156d7d4254a8ad73c32e2a271b5c08
holabisii <ajoseholabisi@gmail.com>
2022-06-03 11:05:08 -0400

    input fault field created and styled

--- front/src/page/Index.tsx
+++ front/src/page/Index.tsx
@@ -1,6 +1,7 @@
 
 import { Header } from "../component/Header"
 import '../css/statscontainer.css'
+import '../css/home.css'
 
 export const Home = () => {
         return (
@@ -8,23 +9,45 @@ export const Home = () => {
                         <div>
                                 <Header />
 
-                                <div>
-                                <div className="stats-container red">
-                                        <i className="fa-solid fa-comments fa-3x icon"></i>
-                                        <div className="counter" data-ceil="15">15</div>
-                                        <h4>Comments</h4>
-                                        </div>
-                                        <div className="stats-container blue">
-                                        <i className="fa-solid fa-circle-question fa-3x icon"></i>
-                                        <div className="counter" data-ceil="260">260</div>
-                                        <h4>Questions</h4>
+                                <div className="stats-container-main" >
+                                        <div className="stats-container red">
+
+                                                <i className="fa fa-exclamation-triangle"></i>
+                                                <div className="counter">200</div>
+                                                <h4>FAULT</h4>
+
                                         </div>
                                         <div className="stats-container orange">
-                                        <i className="fa-solid fa-user fa-3x icon"></i>
-                                        <div className="counter" data-ceil="27">27</div>
-                                        <h4>Users</h4>
+
+                                                <i className="fa fa-exclamation"></i>
+                                                <div className="counter">30</div>
+                                                <h4>SOLUTION</h4>
+
+                                        </div>
+                                        <div className="stats-container blue">
+                                                
+                                                <i className="fa fa-check"></i>
+                                                <div className="counter">50</div>
+                                                <h4>VEFIRIFIED SOLUTION</h4>
+                                                
                                         </div>
                                 </div>
+
+                                <div>
+                                        <h2 className="input_faulth2">POST A HARDWARE FAULT WITH THE PLUS ICON</h2>
+                                        <div className="input_fault">
+                                                <div>
+                                                        <img src="../../undraw_data_input_fxv2.png" alt="input" />
+                                                        
+                                                </div>
+                                                <div>
+                                                        <div className="input_fault plus-circle"></div>
+
+                                                </div>
+                                        </div>
+
+                                </div>
+                                
                                 <h2>this is a new fault</h2>
                                 
                         </div>
