7c06edd5796428e38a28bbd8c94565dc868a3339
holabisii <ajoseholabisi@gmail.com>
2022-06-05 23:26:06 -0400

    tailwind added with a single card post

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@
 import {Home} from './page/Index'
 import {Login} from './page/Login'
 import {CreateFaultForm} from './page/CreateFaultForm'
+import {ViewFaults} from './page/ViewFaults'
 import {Routes, Route} from 'react-router-dom'
 
 
@@ -9,11 +10,11 @@ function App() {
         return (
                 <div>
 
-
                         <Routes>
                                 <Route path="/" element={<Home />} />
                                 <Route path="/login" element={<Login />} />
                                 <Route path="/create" element={<CreateFaultForm />} />
+                                <Route path="/faultsnp" element={<ViewFaults />} />
                         </Routes>
 
                 </div>
