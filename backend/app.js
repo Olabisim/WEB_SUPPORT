d1a66ec7be940abb3b12930a84e95755c13e5f53
holabisii <ajoseholabisi@gmail.com>
2022-06-22 01:10:37 -0400

    mongo connected and dotenv installed

--- backend/app.js
+++ backend/app.js
@@ -11,4 +11,28 @@ const app = express();
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
+
+// unhandled routes
+app.all("*", (req, res) => {
+        res.status(404).json({
+                status: "fail",
+                message: `can't find ${req.originalUrl} on this server`
+        })
+})
+
+
+// global error handler
+
+app.use((err, req, res) => {
+        err.statusCode = err.statusCode || 500;
+        err.status = err.status || "error";
+
+        res.status(err.statusCode).json({
+                status: err.status,
+                message: err.message
+        })
+})
+
+
+
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 import app from './app.js'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 const port = 7000;
+
+dotenv.config({
+        path: './.env'
+})
+
+const MONGO_URI = process.env.MONGO_URI
+
+mongoose.connect(MONGO_URI)
+        .then(connection => console.log("mongodb connected"))
+        .catch(err => console.log(`error::: ${err}`))
 
 app.listen(port, () => {
         console.log("server is listening on port 7000")
